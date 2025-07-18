b653a188b62f8098059806abb9de13a8f2921ba7
Tomi Adenekan <tominekan12@gmail.com>
2025-07-18 13:01:20 -0400

    Added more functionality to the TrieNode

--- pycomplete/trie.py
+++ pycomplete/trie.py
@@ -4,8 +4,9 @@ This gives us the basic content for a suffix trie.
 For runtime analysis purposes, we assume that the number of letters in the alphabet
 is a, the number of words in the trie is n. 
 
-TODO: first we need to develop trie nodes lol
+TODO: THINK ABOUT RUNTIMES LOL
 """
+from trienode import TrieNode
 
 class Trie:
     def __init__(self, content):
@@ -13,10 +14,23 @@ class Trie:
         Assuming size of string is n. O(n)
         Builds and compresses a Trie on all the set of words
         """
+
         words = content.split(" ")
-        words.split()
+        self._root = TrieNode("")
+
+        
 
     def add_word(self, word):
         """
-        Adds a word to the Suffix trie.
+        
+        Adds a word to the Trie Structure.
         """
+
+        curr_node = self._root
+
+        for char in word:
+            if (not curr_node.exists_child(char)):
+                curr_node.add_child(TrieNode(char))
+                curr_node = curr_node.get_node_from_value(char)
+        
+        curr_node.add_child(TrieNode("end"))
--- pycomplete/trienode.py
+++ pycomplete/trienode.py
@@ -2,7 +2,9 @@
 This allows us to create and manipulate Nodes within the Trie structure.
 
 We're assuming that the value of the Node within a specific alphabet.
-This implies that a certain TrieNode should NOT have a child
+This implies that a certain TrieNode should NOT have a child.
+
+For runtime analysis purposes, we're assuming that n = the number of children in the TrieNode.
 """
 
 from typing import Union, Any
@@ -19,7 +21,7 @@ class TrieNode:
         value: Any
             The value of the new TrieNode
         
-        parent: "TrieNode" | None
+        parent: TrieNode | None
             The parent of the current TrieNode
         """
 
@@ -53,7 +55,95 @@ class TrieNode:
     
     def add_child(self, child: "TrieNode"):
         """
-        Adds a child node
+        O(1)
+
+        Adds a child node to this current node. It also automatically sets the parent of
+        this node to this node too.
+
+        Parameters
+        ---
+        child: TrieNode
+            The child TrieNode we want to add
         """
         self._children[child._value] = child
+        child._parent = self
+
+    def remove_child(self, child: "TrieNode"):
+        """
+        O(1)
+
+        Removes `child` from set of children. It also sets the parent of `child` to None. 
+        Raises ValueError if `child` is not a child of the TrieNode
+
+        Parameters
+        ---
+        child: TrieNode
+            The child TrieNode we want to remove
+        """
+        if (child._value not in self._children):
+            raise ValueError("Child does not exist")
+        
+        child._parent = None
+        self._children.pop(child._value)
+    
+
+    def exists_child(self, value: Any):
+        """
+        O(1)
+
+        Checks if a value exists within a child
+
+        Parameters
+        ---
+        value: Any
+            The value we want to check the existence of
+        """
+
+        return value in self._children
+
+    def get_node_from_value(self, value: Any):
+        """
+        O(1)
+
+        Returns the TrieNode corresponding to the value. If there is no child corresponding to value, 
+        we raise ValueError.
+
+        Parameters
+        ---
+        value: Any
+            The value we want to extract
+            
+        """
+        if (value not in self._children):
+            raise ValueError(f"\"{value}\" does not exist as a child")
+        return self._children[value]
+
+
+    def get_children_values(self):
+        """
+        O(n)
+
+        Returns all the values of the children Nodes 
+        """
+
+        return self._children.keys()
+    
+
+    def get_children_nodes(self):
+        """
+        O(n)
+
+        Returns all the children Nodes 
+        """
+
+        return self._children.values()
+
+
+    def is_leaf(self):
+        """
+        O(1)
+
+        Returns whether this current node is a leaf or not.
+        """
+        return len(self._children) == 0
     
